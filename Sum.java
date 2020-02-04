3d0f1abb6d5ad834fc1ec6d1a9b8f10ef34d07a7
HossainImran19 <hossaincse19@gmail.com>
2020-02-05 00:15:22 +0600

    Formula

--- Sum.java
+++ Sum.java
@@ -2,11 +2,12 @@ public class Sum
 {
 	public static void main(String[] args)
 	{
-		int ara[] = {1,2,3,4,5};
+		int ara[] = {1,2,3,4,5,6};
 		int n = ara.length;
 		for(int i = 0; i < n; i++)
 		{
 			System.out.print(ara[i]+" ");
 		}
+		System.out.println("\n"+(n*(n+1)/2));
 	}
 }
