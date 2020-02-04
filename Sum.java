0faef22a0f723642f137013deaa3ced86841572f
HossainImran19 <hossaincse19@gmail.com>
2020-02-05 00:04:32 +0600

    New Commit

---
+++ Sum.java
@@ -0,0 +1,12 @@
+public class Sum
+{
+	public static void main(String[] args)
+	{
+		int ara[] = {1,2,3,4,5};
+		int n = ara.length;
+		for(int i = 0; i < n; i++)
+		{
+			System.out.print(ara[i]+" ");
+		}
+	}
+}
